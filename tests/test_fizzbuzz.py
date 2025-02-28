3253e81ebb0255e8b1c9c608671148441a931cdd
investwl <williangjym@gmail.com>
2025-02-28 14:48:14 +0700

    Fix test discovery path and ensure tests are properly named

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,28 +1,19 @@
+# filepath: /c:/Cawu 4 - Software Engineering/latihan_tdd/tests/test_fizzbuzz.py
 import unittest
-from src.fizzbuzz import fizzbuzz
+from fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), 'Fizz')
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), 'Buzz')
+
     def test_fizzbuzz(self):
-        self.assertTrue(fizzbuzz(3) == 'Fizz')
-        self.assertTrue(fizzbuzz(5) == 'Buzz')
-        self.assertTrue(fizzbuzz(15) == 'FizzBuzz')
-        self.assertFalse(fizzbuzz(4) == 'Fizz')
-        self.assertFalse(fizzbuzz(6) == 'Buzz')
-        self.assertFalse(fizzbuzz(1232131) == 'Fizz')
-        self.assertFalse(fizzbuzz(1232131) == 'Buzz')
-        
-    # def test_string_fizzbuzz(self):
-    #     self.assertFalse(fizzbuzz('hi') == 'Fizz')
+        self.assertEqual(fizzbuzz(15), 'FizzBuzz')
 
-    # def test_decimal_fizzbuzz(self):
-    #     self.assertTrue(fizzbuzz(3.0) == 'Fizz')
-    #     self.assertTrue(fizzbuzz(5.1) == 'Buzz')
-    #     self.assertTrue(fizzbuzz(15.0) == 'FizzBuzz')
-
-    # def test_negative_fizzbuzz(self):
-    #     self.assertTrue(fizzbuzz(-3) == 'Fizz')
-    #     self.assertTrue(fizzbuzz(-5) == 'Buzz')
-    #     self.assertTrue(fizzbuzz(-16) == 'FizzBuzz')
+    def test_none(self):
+        self.assertFalse(fizzbuzz(2))
 
 if __name__ == '__main__':
     unittest.main()
